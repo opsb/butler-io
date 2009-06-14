37279ce881ba2f409627fbb5ad532ee47305fa82
opsb <oliver@opsb.co.uk>
2009-06-14 21:17:49 +0100

    Renamed IOUtil tests to reflect that they use vfs locations

--- src/test/java/uk/co/opsb/pmc/IOUtilsSpec.java
+++ src/test/java/uk/co/opsb/pmc/IOUtilsSpec.java
@@ -2,7 +2,8 @@ package uk.co.opsb.pmc;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
-import static uk.co.opsb.pmc.IOUtils.*;
+import static uk.co.opsb.pmc.IOUtils.bytesFrom;
+import static uk.co.opsb.pmc.IOUtils.textFrom;
 
 import org.junit.Test;
 
@@ -12,12 +13,12 @@ public class IOUtilsSpec {
 	private static final String FILE_LOCATION = "res:uk/co/opsb/pmc/text_file.txt";
 	
 	@Test
-	public void shouldReadText() {
+	public void shouldReadTextFromVfsLocation() {
 		assertThat( textFrom( FILE_LOCATION ), equalTo(FILE_CONTENTS) );
 	}
 	
 	@Test
-	public void shouldReadBytes() {
+	public void shouldReadBytesFromVfsLocation() {
 		assertThat( bytesFrom( FILE_LOCATION ), equalTo(FILE_CONTENTS.getBytes()) );
 	}
 	
